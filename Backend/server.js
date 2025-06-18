5cb07de2e062df43bfab457350f4a6435aad9105
Jayshree Jena <jenajayshree2005@gmail.com>
2025-06-18 19:48:43 +0530

    all done

--- Backend/server.js
+++ Backend/server.js
@@ -9,42 +9,42 @@ const app = express();
 // });
 
 //get a list of 7 jokes 
-app.get('/jokes', (req, res) => {
+app.get('/api/jokes', (req, res) => {
     const jokes = [
         {
             id: 1,
             title: 'jas',
-            content :'a b c d'
+            content :'Why do programmers prefer dark mode? Because light attracts bugs'
         },
         {
             id: 2,
             title: 'another joke',
-            content :'a b c d'
+            content :'What is the unit of power?Student: Watt?Teacher: I said, what is the unit of power?!Student: WATT!!'
         },
         {
             id: 3,
             title: '3rd joke',
-            content :'a b c d'
+            content :'Why don’t skeletons fight each other? They don’t have the guts'
         },
         {
             id: 4,
             title: '4th joke',
-            content :'a b c d'
+            content :'Me after 5 minutes of studying: "I need a break."Me after the break: "I need a nap."'
         },
         {
             id: 5,
             title: '5th joke',
-            content :'a b c d'
+            content :'Why did the computer go to therapy? It had too many bytes from past relationships'
         },
         {
             id: 6,
             title: '6th joke',
-            content :'a b c d'
+            content :'Why did the student eat their homework? Because the teacher said it was a piece of cake.'
         },
         {
             id: 7,
             title: '7th joke',
-            content :'x y s d'
+            content :'What do you call an AI that tells jokes? ChatGPT.'
         }  
     ];
     res.send(jokes);
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // Adjust the target to your backend server
+      },
+    },
+  },
   plugins: [react()],
 })
